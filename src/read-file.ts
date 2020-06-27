ca0b0607c3ca231546ee6cceff88eddb23478ce1
Shinichi Kawamura <sttaf34@gmail.com>
2020-06-27 16:52:54 +0900

    tsconfig.json の設定変更

---
+++ src/read-file.ts
@@ -0,0 +1,54 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Handler,
+  Context,
+  Callback
+} from "aws-lambda"
+import { createReadStream } from "fs"
+
+// デプロイ後のエンドポイント
+// https://sttaf34-netlify-functions.netlify.app/.netlify/functions/read-file
+
+export const handler: Handler = (
+  event: APIGatewayProxyEvent,
+  context: Context,
+  callback: Callback
+): void => {
+  // GET
+  // curl -X GET http://localhost:9000/.netlify/functions/read-file
+
+  // ファイル読みこんで表示する
+  // const contents = fs.readFileSync(require.resolve('./assets/test.txt'))
+
+
+
+  // JavaScriptのメモを書くときに使えるように、
+  // Access-Control-Allow-Origin 設定したものを用意しとく
+  if (event.httpMethod === "GET") {
+    const randomNumber = Math.floor(Math.random() * 10000)
+    const result: APIGatewayProxyResult = {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "text/plain",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: `${randomNumber}\n`
+    }
+    callback(null, result)
+    return
+  }
+
+  // 未対応のHTTPメソッド
+  // curl -i -X PUT http://localhost:9000/.netlify/functions/allow-origin
+  const result: APIGatewayProxyResult = {
+    statusCode: 405,
+    headers: {
+      // eslint-disable-next-line prettier/prettier
+      "Allow": "GET",
+      "Content-Type": "text/plain"
+    },
+    body: "Method Not Allowed\n"
+  }
+  callback(null, result)
+}
