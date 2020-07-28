256aa45d9f7008d8f0aab18684b6a67ed705bde8
Shinichi Kawamura <sttaf34@gmail.com>
2020-07-29 01:37:37 +0900

    設定ファイル修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,40 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "browser": true
+  env: {
+    es6: true,
+    browser: true,
   },
-  "extends": [
+  extends: [
     "airbnb-base",
     "plugin:@typescript-eslint/recommended",
     "prettier",
     "prettier/@typescript-eslint",
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": "./tsconfig.json"
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
   },
-  "plugins": ["@typescript-eslint", "prettier"],
-  "settings": {
+  plugins: ["@typescript-eslint", "prettier"],
+  settings: {
     "import/resolver": {
-      "node": {
-        "extensions": [".js", ".ts"]
-      }
-    }
+      node: {
+        extensions: [".js", ".ts"],
+      },
+    },
   },
-  "rules": {
+  rules: {
     "no-console": "off",
-    "prettier/prettier": ["error", { "semi": false } ],
+    "prettier/prettier": ["error", { semi: false }],
 
     // https://stackoverflow.com/questions/59265981/typescript-eslint-missing-file-extension-ts-import-extensions
     "import/extensions": [
-      "error", "ignorePackages", { "ts": "never", "tsx": "never" }
+      "error",
+      "ignorePackages",
+      { ts: "never", tsx: "never" },
     ],
 
     // export default は基本使わないとする
     // https://engineering.linecorp.com/ja/blog/you-dont-need-default-export/
     "import/no-default-export": "error",
     "import/prefer-default-export": "off",
-  }
+  },
 }
