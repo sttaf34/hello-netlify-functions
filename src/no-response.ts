f6ff881debe908a272a8707e92c5a64504d89dcb
Shinichi Kawamura <sttaf34@gmail.com>
2020-07-29 02:14:02 +0900

    テスト用としてレスポンスを返さない関数を追加

---
+++ src/no-response.ts
@@ -0,0 +1,9 @@
+import { Handler } from "aws-lambda"
+
+// デプロイ後のエンドポイント
+// https://sttaf34-netlify-functions.netlify.app/.netlify/functions/no-response
+// curl -m 2 http://localhost:9000/.netlify/functions/no-response
+
+export const handler: Handler = (): void => {
+  //
+}
